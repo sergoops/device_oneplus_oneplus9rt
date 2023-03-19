65fc8173bab3dec21e8c5881c61ece4668df9808
Omkar Chandorkar <gotenksIN@aospa.co>
2023-03-19 15:26:34 +0530

    oneplus9rt: Switch to QTI BootCtrl 1.2 implementation
    Introduces getActiveBootSlot
    
    Change-Id: Ief989bb902b0cea171c47430663552cb3abb80ee

--- device.mk
+++ device.mk
@@ -98,9 +98,9 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Boot
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 # Camera
 PRODUCT_COPY_FILES += \
