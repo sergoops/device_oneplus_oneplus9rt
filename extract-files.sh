f39e44b924badf2b4628b002f34a59405a65c9ea
sergoops <sergoopsmoskal@gmail.com>
2023-04-02 12:47:32 +0300

    oneplus9rt: Update proprietary-files from MT2110 13.1.0.100 (CN01) * Import blobs needed for 9rt * Update shasums * Pinned blobs taken from ColorOS 100

--- extract-files.sh
+++ extract-files.sh
@@ -65,7 +65,7 @@ function blob_fixup() {
             "${SIGSCAN}" -p "45 B8 05 94" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            "${SIGSCAN}" -p "23 0B 00 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "27 0B 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
     esac
 }
