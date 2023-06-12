39001b382184ede93660a001526469dd415fb0e0
sergoops <sergoopsmoskal@gmail.com>
2023-06-13 01:11:50 +0300

    oneplus9rt: Update to MT2110 ColorOS 100

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -38,10 +38,10 @@ void vendor_load_properties() {
     switch (prjname) {
         case 20820: // CN
             OverrideProperty("ro.product.product.model", "MT2110");
-            OverrideProperty("ro.product.product.device", "OP5154L1");
             break;
         case 20821: // IN
             OverrideProperty("ro.product.product.model", "MT2111");
+            OverrideProperty("ro.product.product.device", "OP5155L1");
             break;
         default:
             LOG(ERROR) << "Unexpected project name: " << prjname;
